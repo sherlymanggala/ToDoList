3b1fc2403808d170b017b9b53a70dea292425683
sherlymanggala <sherly_manggala@yahoo.com>
2019-09-01 01:15:21 +1000

    First commit

---
+++ app/src/main/java/comp5216/sydney/edu/au/todolist/ToDoItemDB.java
@@ -0,0 +1,30 @@
+package comp5216.sydney.edu.au.todolist;
+
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {ToDoItem.class}, version = 1, exportSchema = false)
+public abstract class ToDoItemDB extends RoomDatabase {
+    private static final String DATABASE_NAME = "todoitem_db";
+    private static ToDoItemDB DBINSTANCE;
+
+    public abstract ToDoItemDao toDoItemDao();
+
+    public static ToDoItemDB getDatabase(Context context) {
+        if (DBINSTANCE == null) {
+            synchronized (ToDoItemDB.class) {
+                DBINSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                        ToDoItemDB.class, DATABASE_NAME).build();
+            }
+        }
+        return DBINSTANCE;
+    }
+
+    public static void destroyInstance() {
+        DBINSTANCE = null;
+    }
+}
